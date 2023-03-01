5ed6d1a3864764602314c7a89a75ae1ba9a41ce5
渡辺友美 <tncnkncn.0329@gmail.com>
2023-03-01 09:35:57 +0900

    ユーザー認証機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  #このメソッドをも使うことで、ログインしている状態の時とログアウト状態の時で表示が変わるような実装ができる。ログイン状態になければログイン画面に案内される
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
